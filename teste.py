1c698a4f0961bf0bc154e9ea1db154100f937e78
Alexandre Siqueira <alexandre.s.s.c@hotmail.com>
2026-04-19 17:23:47 -0300

    feat: Implementa fluxo de preenchimento da planilha molde

--- teste.py
+++ teste.py
@@ -1,12 +1,5 @@
-def calculate_triangle_area(base, height):
-    """Calculate the area of a triangle given base and height."""
-    area = (base * height) / 2
-    return area
+import pandas as pd
 
-# Get user input
-base = float(input("Enter the base of the triangle: "))
-height = float(input("Enter the height of the triangle: "))
 
-# Calculate and display the area
-area = calculate_triangle_area(base, height)
-print(f"The area of the triangle is: {area}")
+df = pd.read_excel(r"data\planilha_cadastro.xlsx")
+df
